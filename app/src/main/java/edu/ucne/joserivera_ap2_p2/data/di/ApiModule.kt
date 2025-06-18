90d4487eb7bd8744413b12cbed343c32731966ee
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-18 19:59:26 -0400

    ApiModule.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p2/data/di/ApiModule.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p2/data/di/ApiModule.kt
@@ -1,4 +1,54 @@
 package edu.ucne.joserivera_ap2_p2.data.di
 
-class ApiModule {
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.joserivera_ap2_p2.data.remote.GitHubApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApiModule {
+    private const val BASE_URL = "https://api.github.com/"
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .add(DateAdapter())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGitHubApi(moshi: Moshi, client: OkHttpClient): GitHubApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(GitHubApi::class.java)
+    }
 }
