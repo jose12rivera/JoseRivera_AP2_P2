4d25a906d0c68e40ecebffd9bf0d12d47b3921e5
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-19 08:23:21 -0400

    GitHubRepository.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p2/data/respository/GitHubRepository.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p2/data/respository/GitHubRepository.kt
@@ -1,7 +1,6 @@
 package edu.ucne.joserivera_ap2_p2.data.respository
 
-
-import edu.ucne.joserivera_ap2_p2.data.remote.RemoteDataSource
+import edu.ucne.joserivera_ap2_p2.data.remote.GitHubApi
 import edu.ucne.joserivera_ap2_p2.data.remote.Resource
 import edu.ucne.joserivera_ap2_p2.data.remote.dto.RepositoryDto
 import kotlinx.coroutines.flow.Flow
@@ -9,15 +8,15 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GitHubRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val api: GitHubApi
 ) {
-    suspend fun getRepositories(username: String): Flow<Resource<List<RepositoryDto>>> = flow {
+    fun getRepositories(username: String): Flow<Resource<List<RepositoryDto>>> = flow {
         emit(Resource.Loading())
         try {
-            val repositories = remoteDataSource.getRepositories(username)
-            emit(Resource.Success(repositories))
+            val result = api.getRepositories(username)
+            emit(Resource.Success(result))
         } catch (e: Exception) {
-            emit(Resource.Error("Error al obtener repositorios: ${e.message}"))
+            emit(Resource.Error(e.localizedMessage ?: "Error al obtener los repositorios"))
         }
     }
 }
