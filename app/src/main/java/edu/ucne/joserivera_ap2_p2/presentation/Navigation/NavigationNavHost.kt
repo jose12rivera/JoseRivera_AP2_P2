6686274c680669ae86aa1276dbbbc77bb074975e
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-19 08:24:25 -0400

    NavigationNavHost.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p2/presentation/Navigation/NavigationNavHost.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p2/presentation/Navigation/NavigationNavHost.kt
@@ -1,25 +1,54 @@
-// edu.ucne.joserivera_ap2_p2.presentation.navigation.AppNavigation.kt
 package edu.ucne.joserivera_ap2_p2.presentation.navigation
 
-
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.joserivera_ap2_p2.data.remote.dto.RepositoryDto
 import edu.ucne.joserivera_ap2_p2.presentation.HomeScreen
+import edu.ucne.joserivera_ap2_p2.presentation.Navigation.Screen
 import edu.ucne.joserivera_ap2_p2.presentation.respository.RepositoryListScreen
 import edu.ucne.joserivera_ap2_p2.presentation.respository.RepositoryScreen
 import edu.ucne.joserivera_ap2_p2.presentation.respository.RepositoryViewModel
 
 @Composable
 fun AppNavigation(navController: NavHostController, viewModel: RepositoryViewModel) {
-    NavHost(navController = navController, startDestination = "home") {
-        composable("home") {
-            HomeScreen(goToRepository = {
-                navController.navigate("repositoryList")
-            })
+    NavHost(navController = navController, startDestination = Screen.HomeScreen) {
+        composable<Screen.HomeScreen> {
+            HomeScreen(
+                goToRepository = {
+                    navController.navigate(Screen.RepositoryList("joserivera"))
+                }
+            )
         }
 
+        composable<Screen.RepositoryList> {
+            val args = it.toRoute<Screen.RepositoryList>()
+            RepositoryListScreen(username = args.username, viewModel = viewModel, goToRepository = { repo ->
+                    navController.navigate(
+                        Screen.RepositoryDetail(
+                            name = repo.name,
+                            desc = repo.description,
+                            url = repo.htmlUrl
+                        )
+                    )
+                }, onDrawer = {
+                    // puedes añadir lógica de Drawer si es necesario
+                })
+        }
+
+        composable<Screen.RepositoryDetail> {
+            val args = it.toRoute<Screen.RepositoryDetail>()
+            RepositoryScreen(
+                repository = RepositoryDto(
+                    name = args.name,
+                    description = args.desc,
+                    htmlUrl = args.url
+                ),
+                navController = navController
+            )
+        }
     }
 }
-
