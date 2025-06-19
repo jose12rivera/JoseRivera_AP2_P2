3f418c08b874dd54c560b7656db76b33c6ec4fc7
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-19 08:24:43 -0400

    RemoteDataSource.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p2/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p2/data/remote/RemoteDataSource.kt
@@ -1,3 +1,4 @@
+// RemoteDataSource.kt
 package edu.ucne.joserivera_ap2_p2.data.remote
 
 import edu.ucne.joserivera_ap2_p2.data.remote.dto.RepositoryDto
@@ -7,6 +8,6 @@ class RemoteDataSource @Inject constructor(
     private val gitHubApi: GitHubApi
 ) {
     suspend fun getRepositories(username: String): List<RepositoryDto> {
-        return gitHubApi.listRepos(username)
+        return gitHubApi.getRepositories(username)
     }
 }
