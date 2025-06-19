f4e452a62a4e7a7d9d686bf1a553d3f28af07068
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-18 20:00:10 -0400

    Screen.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p2/presentation/Navigation/Screen.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p2/presentation/Navigation/Screen.kt
@@ -1,2 +1,14 @@
 package edu.ucne.joserivera_ap2_p2.presentation.Navigation
 
+import kotlinx.serialization.Serializable
+
+sealed class Screen {
+    @Serializable
+    data object HomeScreen : Screen()
+    @Serializable
+    data object RepositoryList : Screen()
+
+    @Serializable
+    data class RepositoryDetail(val id: Int) : Screen()
+
+}
