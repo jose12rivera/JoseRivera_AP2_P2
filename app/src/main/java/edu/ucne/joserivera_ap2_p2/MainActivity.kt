c8b27fab0537d09b3ac5cf354f7f7cc493133dd2
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-19 08:23:55 -0400

    MainActivity.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p2/MainActivity.kt
@@ -11,37 +11,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.joserivera_ap2_p2.presentation.navigation.AppNavigation
+import edu.ucne.joserivera_ap2_p2.presentation.respository.RepositoryViewModel
 import edu.ucne.joserivera_ap2_p2.ui.theme.JoseRivera_AP2_P2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             JoseRivera_AP2_P2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                val viewModel: RepositoryViewModel = hiltViewModel()
+
+                AppNavigation(navController = navController, viewModel = viewModel)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JoseRivera_AP2_P2Theme {
-        Greeting("Android")
-    }
-}
