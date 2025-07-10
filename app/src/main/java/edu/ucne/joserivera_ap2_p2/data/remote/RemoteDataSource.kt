6b3cf4ee7b55613e8ae46584859609262fc571b7
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-07-10 16:19:30 -0400

    RemoteDataSource.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p2/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p2/data/remote/RemoteDataSource.kt
@@ -1,6 +1,6 @@
-// RemoteDataSource.kt
 package edu.ucne.joserivera_ap2_p2.data.remote
 
+import edu.ucne.joserivera_ap2_p2.data.remote.dto.ContribuidoreDto
 import edu.ucne.joserivera_ap2_p2.data.remote.dto.RepositoryDto
 import javax.inject.Inject
 
@@ -8,6 +8,20 @@ class RemoteDataSource @Inject constructor(
     private val gitHubApi: GitHubApi
 ) {
     suspend fun getRepositories(username: String): List<RepositoryDto> {
-        return gitHubApi.getRepositories(username)
+        val response = gitHubApi.getRepositories(username)
+        if (response.isSuccessful) {
+            return response.body() ?: emptyList()
+        } else {
+            throw Exception("Error: ${response.code()} ${response.message()}")
+        }
+    }
+
+    suspend fun getContributors(owner: String, repo: String): List<ContribuidoreDto> {
+        val response = gitHubApi.getContributors(owner, repo)
+        if (response.isSuccessful) {
+            return response.body() ?: emptyList()
+        } else {
+            throw Exception("Error: ${response.code()} ${response.message()}")
+        }
     }
 }
