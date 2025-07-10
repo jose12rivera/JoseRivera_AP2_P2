6aa730ed5752556df17d75c49a3f48a8df4ebd04
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-07-10 16:17:31 -0400

    Screen.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p2/presentation/Navigation/Screen.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p2/presentation/Navigation/Screen.kt
@@ -6,13 +6,14 @@ sealed class Screen {
     @Serializable
     data object HomeScreen : Screen()
     @Serializable
+    data class ContributorList(val owner: String, val repoName: String) : Screen()
+    @Serializable
     data class RepositoryList(val username: String) : Screen()
 
     @Serializable
-    data class RepositoryDetail(
+    data class ViewRepository(
         val name: String,
-        val desc: String?,
+        val desc: String? = null,
         val url: String
     ) : Screen()
-
 }
