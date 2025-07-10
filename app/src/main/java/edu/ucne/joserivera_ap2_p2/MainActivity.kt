4d49eeef4f1a2c9c53780870051cb16bd745c5d3
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-07-10 16:17:20 -0400

    MainActivity.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p2/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
@@ -28,9 +23,15 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 val viewModel: RepositoryViewModel = hiltViewModel()
 
-                AppNavigation(navController = navController, viewModel = viewModel)
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation(
+                        navController = navController,
+                        repositoryViewModel = viewModel
+                    )
+                }
             }
         }
     }
 }
-
