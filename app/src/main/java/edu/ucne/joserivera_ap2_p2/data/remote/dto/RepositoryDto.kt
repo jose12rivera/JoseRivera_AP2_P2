125517952a26db612af689bbaa3ba7d66bddc08f
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-07-10 16:19:15 -0400

    RepositoryDto.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p2/data/remote/dto/RepositoryDto.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p2/data/remote/dto/RepositoryDto.kt
@@ -3,7 +3,9 @@ package edu.ucne.joserivera_ap2_p2.data.remote.dto
 import com.squareup.moshi.Json
 
 data class RepositoryDto(
+    val id: Int? = null,
     @Json(name = "name") val name: String,
     @Json(name = "description") val description: String?,
-    @Json(name = "html_url") val htmlUrl: String
+    @Json(name = "html_url") val htmlUrl: String,
+    @Transient var isLocal: Boolean = false // Ignorar en la serialización
 )
